17c28c1dee944857c1fe3271988212d446cf1ab9
raviteja <teja4360@gmail.com>
2023-08-09 07:29:28 +0530

    Random txt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc"{
-  source= "git::https://github.com/tej4360/tf-vpc-module.git"
+  source= "git::https://github.com/tej4360/tf-vpc-module2.git"
   for_each = var.vpc
   cidr_block=each.value["cidr_block"]
 }
