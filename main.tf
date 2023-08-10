7df107efaee2da53f4f45f66e0f49eddfb7fea12
raviteja <teja4360@gmail.com>
2023-08-10 07:38:59 +0530

    Random txt

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc_block"{
-  for_each = var.vpc
   source = "git::https://github.com/tej4360/tf-vpc-module2.git"
+
+  for_each = var.vpc
   cidr_block = each.value["cidr_block"]
 }
--- vars.tf
+++ vars.tf
@@ -1,4 +1,2 @@
 
-variable "vpc" {}
-
 variable "cidr_block" {}
